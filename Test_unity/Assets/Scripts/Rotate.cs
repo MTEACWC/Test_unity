7aae6992de701c16cdf63d228e9f86b7bffdf6db
S_XGX1 <S_xgx1@outlook.com>
2018-10-17 11:57:33 +0800

    旋转功能

---
+++ Test_unity/Assets/Scripts/Rotate.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public int Speed { get; set; } = 10;
+    void Update() => transform.Rotate(Vector3.up * Time.deltaTime * Speed);
+}
